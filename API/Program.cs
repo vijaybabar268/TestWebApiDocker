d76405f7732cfa05fc1cf6dd97f02c39083aefa8
vijaybabar <babarvm@gmail.com>
2023-12-09 18:48:46 +0530

    Initial commit again

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,24 @@
+// public class Program
+// {
+//     const string envName = "dev1";
+//     public static void Main(string[] args)
+//     {
+//         CreateHostBuilder(args).Build().Run();
+//     }
+
+//     public static IHostBuilder CreateHostBuilder(string[] args)
+//     {
+//         return Host.CreateDefaultBuilder(args)
+//             .ConfigureAppConfiguration((a, config) =>
+//             {
+//                 config.AddJsonFile($"appsettings{envName}.json");
+//             });
+//     }
+// }
+
+
+
+const string envName = "dev1";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,10 +28,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var appsettings = builder.Configuration.AddJsonFile($"appsettings{envName}.json", true, true);
+
 var app = builder.Build();
 
+var env = app.Configuration.GetSection("URLs").GetSection("env").Value;
+var APIUrl = app.Configuration.GetSection("URLs").GetSection("APIUrl").Value;
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsStaging())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
